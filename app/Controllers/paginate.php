a3544290e3dfa75c185c475168078a7de197f07e
Ant-DeLaT <186017554+Ant-DeLaT@users.noreply.github.com>
2025-01-28 13:14:00 +0100

    Added custom_pagination

--- app/Controllers/paginate.php
+++ app/Controllers/paginate.php
@@ -5,9 +5,17 @@ use App\Models\UserModel;
 class Paginate extends  BaseController{
     public function index(){
         $userModel=new UserModel();
+        $name=$this->request->getVar('name'); //Obtener el término de búsqueda desde el dominio
+        // Aplicar filtro si se introduce un nombre
+        if($name){
+            $query=$userModel->like('name',$name);
+        }
+        
+        // Configuración
         $perPage=10; //Número de elementos por página
-        $data['users']=$userModel->paginate($perPage);
+        $data['users']=$query->paginate($perPage);
         $data['pager']=$userModel->pager;
+        $data['name']=$name;
         return view("user_listView2",$data);
     }
 }
