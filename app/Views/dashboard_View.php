3a3197bc19fab459873184117988cb597e97acc2
Ant-DeLaT <186017554+Ant-DeLaT@users.noreply.github.com>
2025-02-24 14:22:44 +0100

    24/02/25

--- app/Views/dashboard_View.php
+++ app/Views/dashboard_View.php
@@ -43,43 +43,47 @@
 
 
 
-<body>
+<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed" style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
   <!-- Sidebar -->
   <div class="d-flex flex-column flex-root">
+        <div>
             <!--begin::Page-->
             <div class="page d-flex flex-row flex-column-fluid">
                 <!--begin::Aside-->
                 <div id="kt_aside" class="aside aside-dark aside-hoverable drawer drawer-start drawer-on" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_mobile_toggle">
                     <!--begin::Brand-->
                     <div class="aside-logo flex-column-auto" id="kt_aside_logo">
-                        <!--begin::Logo-->
-                        <a href="">
-                            <!-- ../../demo1/dist/index.html -->
-                            <img alt="Logo" src="assets/media/logos/logo-1-dark.svg" class="h-25px logo" />
-                        </a>
-                        <!--end::Logo-->
-                        <!--begin::Aside toggler-->
-                        <div id="kt_aside_toggle" class="btn btn-icon w-auto px-0 btn-active-color-primary aside-toggle" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="aside-minimize">
-                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr079.svg-->
-                            <span class="svg-icon svg-icon-1 rotate-180">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path opacity="0.5" d="M14.2657 11.4343L18.45 7.25C18.8642 6.83579 18.8642 6.16421 18.45 5.75C18.0358 5.33579 17.3642 5.33579 16.95 5.75L11.4071 11.2929C11.0166 11.6834 11.0166 12.3166 11.4071 12.7071L16.95 18.25C17.3642 18.6642 18.0358 18.6642 18.45 18.25C18.8642 17.8358 18.8642 17.1642 18.45 16.75L14.2657 12.5657C13.9533 12.2533 13.9533 11.7467 14.2657 11.4343Z" fill="black" />
-                                    <path d="M8.2657 11.4343L12.45 7.25C12.8642 6.83579 12.8642 6.16421 12.45 5.75C12.0358 5.33579 11.3642 5.33579 10.95 5.75L5.40712 11.2929C5.01659 11.6834 5.01659 12.3166 5.40712 12.7071L10.95 18.25C11.3642 18.6642 12.0358 18.6642 12.45 18.25C12.8642 17.8358 12.8642 17.1642 12.45 16.75L8.2657 12.5657C7.95328 12.2533 7.95328 11.7467 8.2657 11.4343Z" fill="black" />
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->
-                        </div>
-                        <!--end::Aside toggler-->
-                    </div>
+						<!--begin::Logo-->
+						<a href="#">
+							<img alt="Logo" src="assets/media/logos/logo-1-dark.svg" class="h-25px logo" />
+						</a>
+						<!--end::Logo-->
+						<!--begin::Aside toggler-->
+						<div id="kt_aside_toggle" class="btn btn-icon w-auto px-0 btn-active-color-primary aside-toggle" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="aside-minimize">
+							<!--begin::Svg Icon | path: icons/duotune/arrows/arr079.svg-->
+							<span class="svg-icon svg-icon-1 rotate-180">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path opacity="0.5" d="M14.2657 11.4343L18.45 7.25C18.8642 6.83579 18.8642 6.16421 18.45 5.75C18.0358 5.33579 17.3642 5.33579 16.95 5.75L11.4071 11.2929C11.0166 11.6834 11.0166 12.3166 11.4071 12.7071L16.95 18.25C17.3642 18.6642 18.0358 18.6642 18.45 18.25C18.8642 17.8358 18.8642 17.1642 18.45 16.75L14.2657 12.5657C13.9533 12.2533 13.9533 11.7467 14.2657 11.4343Z" fill="black" />
+									<path d="M8.2657 11.4343L12.45 7.25C12.8642 6.83579 12.8642 6.16421 12.45 5.75C12.0358 5.33579 11.3642 5.33579 10.95 5.75L5.40712 11.2929C5.01659 11.6834 5.01659 12.3166 5.40712 12.7071L10.95 18.25C11.3642 18.6642 12.0358 18.6642 12.45 18.25C12.8642 17.8358 12.8642 17.1642 12.45 16.75L8.2657 12.5657C7.95328 12.2533 7.95328 11.7467 8.2657 11.4343Z" fill="black" />
+								</svg>
+							</span>
+							<!--end::Svg Icon-->
+						</div>
+						<!--end::Aside toggler-->
+					</div>
                     <!--end::Brand-->
                     
                     <!--begin::Aside menu-->
                     <div class="aside-menu flex-column-fluid">
+                        
                         <!--begin::Aside Menu-->
                         <div class="hover-scroll-overlay-y my-5 my-lg-5" id="kt_aside_menu_wrapper" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer" data-kt-scroll-wrappers="#kt_aside_menu" data-kt-scroll-offset="0">
+                            
                             <!--begin::Menu-->
                             <div class="menu menu-column menu-title-gray-800 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500" id="#kt_aside_menu" data-kt-menu="true">
+                                
                                 <div class="menu-item">
+                                    
                                     <!-- Upper -->
                                     <div class="menu-content pt-8 pb-2">
                                         <span class="menu-section text-muted text-uppercase fs-8 ls-1">Apps</span>
@@ -153,7 +157,7 @@
                     <!--end::Aside menu-->
                     <!--begin::Footer-->
                     <div class="aside-footer flex-column-auto pt-5 pb-7 px-5" id="kt_aside_footer">
-                        <a href="#" class="btn btn-custom btn-primary w-100" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click" title="">
+                        <a class="btn btn-custom btn-primary w-100" href="#" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click" title='Click here to access your user data!'>
                             <span class="btn-label">Bienvenido
                                 <?php
                                 $session=session();
@@ -171,6 +175,8 @@
                             ?>
                             </span>
 
+                           
+
         <!-- php echo cambio  -->
                             <!--begin::Svg Icon | path: icons/duotune/general/gen005.svg-->
                             <span class="svg-icon btn-icon svg-icon-2">
@@ -184,6 +190,7 @@
                     </div>
                     <!--end::Footer-->
                 </div>
+      
             </div>
                 <!--end::Aside-->
                 <!--begin::Wrapper-->
